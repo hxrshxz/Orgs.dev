1d5a79869d83df6f5d9b7b19532fb01adbd604d6
Harsh Kumar <harshmastic@gmail.com>
2025-03-09 16:37:18 +0530

    add sidebar , topbar

---
+++ Orgs/src/components/SideBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/sidebar.css'
+
+const SideBar = () => {
+  return (
+    <div className='sidebar-container'>
+        <div className='sidebar-header'>
+            <h2>Orgs.dev</h2>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
---
+++ Orgs/src/components/TopBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles/topbar.css'
+
+const TopBar = () => {
+  return (
+    <div className='topbar'>TopBar</div>
+  )
+}
+
+export default TopBar
